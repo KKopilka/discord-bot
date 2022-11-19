293591892cb1aeabbe28d5f8a12b82d4ca8f9316
Yana <yana.stepanckova@yandex.ru>
2022-11-19 23:49:41 +0300

    read channel emoji

--- cmd/trash/main.go
+++ cmd/trash/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-var discordBotToken = "MTAwMjI4MTI1MzM5Mjg4Nzg3OQ.GnmLD-.irU9_tc-iOxdySzpnrk6QGZC3CtJTRRZWNNIpE"
+var discordBotToken string
+
+const WasteBasketEmoji = "🗑"
 
 func main() {
+	err := readBotToken()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
 	goBot, err := discordgo.New("Bot " + discordBotToken)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -56,19 +67,39 @@ func removeTrash(goBot *discordgo.Session, guildID string) error {
 			}
 
 			if len(messages) > 3 {
+				messageToDelete := []string{}
 				for _, message := range messages {
-					if err := goBot.ChannelMessageDelete(channel.ID, message.ID); err != nil {
-						fmt.Println(err.Error())
-						continue
-					}
+					messageToDelete = append(messageToDelete, message.ID)
+				}
+
+				if err := goBot.ChannelMessagesBulkDelete(channel.ID, messageToDelete); err != nil {
+					fmt.Println(err.Error())
+					continue
 				}
 			}
 		}
 	}
-
 	return nil
 }
 
 func IsChannelNeedToClean(channelName string) bool {
-	return channelName == "trash"
+	if strings.Index(channelName, WasteBasketEmoji) >= 0 {
+		return true
+	}
+
+	if strings.Index(channelName, "trash") >= 0 {
+		return true
+	}
+	return false
+}
+
+func readBotToken() error {
+	file, err := ioutil.ReadFile("bot-token")
+
+	if err != nil {
+		return err
+	}
+
+	discordBotToken = string(file)
+	return nil
 }
